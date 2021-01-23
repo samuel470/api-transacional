6b9b046911d7072007ac6d8e0f1f0b91c037d1d2
Samuel Mota <sssamu.silva@hotmail.com>
2021-01-23 11:48:59 -0300

    Primeiro commit

---
+++ src/main/java/com/mota/samuel/desafiotransacoes/DesafioTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.mota.samuel.desafiotransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mota/samuel/desafiotransacoes/DesafioTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mota.samuel.desafiotransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
